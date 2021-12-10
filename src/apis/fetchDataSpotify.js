69b6910752b329aad8fc69541a823bc2f8f4f673
Silviu Bocsa <silviubocsa@gmail.com>
2021-12-10 05:10:04 +0200

    implemented discover

---
+++ src/apis/fetchDataSpotify.js
@@ -0,0 +1,42 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+	clientId: '3a2a790abc4d4c26a02a7cfe404d86d9',
+	clientSecret: '4f5923cd46a24424a59053ddff2de1fa',
+	redirectUri: 'http://localhost:3000'
+})
+
+export function getRelatedArtist(artistId) {
+	const request = require('request'); // "Request" library
+	const client_id = '3a2a790abc4d4c26a02a7cfe404d86d9'; // Your client id
+	const client_secret = '4f5923cd46a24424a59053ddff2de1fa'; // Your secret
+	const authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+		},
+		form: {
+			grant_type: 'client_credentials'
+		},
+		json: true
+	};
+	return new Promise(function (resolve, reject) {
+		request.post(authOptions, function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				// use the access token to access the Spotify Web API
+				const token = body.access_token;
+				spotifyApi.setAccessToken(token);
+
+				return spotifyApi.getArtistRelatedArtists(`${artistId}`)
+					.then(function (data) {
+						console.log(data.body);
+						resolve(data.body)
+
+					}, function (error) {
+						console.log(error)
+						reject(error)
+					})
+			}
+		});
+	})
+}
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,17 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { genresAPI, getSearchDetailsAPI } from "../apis/fetchData";
+import { Link as LinkPrime, useHistory } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+
 
 
 
 function Discover(props) {
+	const params = new URLSearchParams(window.location.search)
+	const artistParam = params.get('artist');
+	const pageParam = params.get('page');
+	const styleParam = params.get('style');
+	const genreParam = params.get('genre');
 
+	const [ artist, setArtist ] = useState([]);
+	const [ searchArtist, setSearchArtist ] = useState(artistParam || "");
+	const [ searchGenre, setSearchGenre ] = useState(genreParam || "");
+	const [ searchStyle, setSearchStyle ] = useState(styleParam || "");
+	const [ searchStyles, setSearchStyles ] = useState("");
+	const [ currentPage, setCurrentPage ] = useState(parseInt(pageParam) || 1);
+	const [ pagesNumber, setPagesNumber ] = useState();
+	const [ resultsNumber, setResultsNumber ] = useState();
+	const [ isLoading, setIsLoading ] = useState(true);
 
+	const genres = genresAPI();
+	let history = useHistory();
+
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter" && event.target.value === "") {
+			setSearchArtist(event.target.value);
+			setCurrentPage(1);
+		} else if (event.key === "Enter") {
+			setSearchArtist(event.target.value);
+		}
+	};
+
+	const handlePages = (event, value) => {
+		setCurrentPage(value);
+	};
+
+	useEffect(() => {
+		history.push(`/discover?artist=${searchArtist}&page=${currentPage}`)
+		getSearchDetailsAPI(searchArtist, searchGenre, searchStyle, currentPage).then((data) => {
+			setResultsNumber(data.pagination?.items);
+			setArtist(data.results);
+			setPagesNumber(data.pagination?.pages);
+			setIsLoading(false);
+		});
+	}, [searchGenre, searchArtist, searchStyle, currentPage, history]);
 
 	return (
 		<div className="discover-container">
 			<div className="discover-card-title">
-
+				<picture>
+					<img src="https://images.pexels.com/photos/5569968/pexels-photo-5569968.jpeg" className="img-fluid" alt="..."/>
+				</picture>
+				<SearchBar/>
 			</div>
 			<div className="discover-content">
+
 			</div>
 		</div>
 	);
